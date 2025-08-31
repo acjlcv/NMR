cf7069f9502e50d120695ae7a7633f9945308a60
Arnold <47092234+acjlcv@users.noreply.github.com>
2025-08-31 14:22:58 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import unittest
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
 
 CUDA_FLAGS = []
-INSTALL_REQUIREMENTS = ["torch", "scikit-image", "numpy"]
+INSTALL_REQUIREMENTS = ["torch", "scikit-image", "numpy", "cython"]
 
 def test_all():
     test_loader = unittest.TestLoader()
